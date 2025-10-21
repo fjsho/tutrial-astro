1dd0a8e544b87d1f03bae8f89e68f07eaa85c03f
fjsho <81564274+fjsho@users.noreply.github.com>
2025-10-22 05:31:12 +0900

    add: Preact統合を追加して挨拶を表示する

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://tutrial-astro.netlify.app"
+  site: "https://tutrial-astro.netlify.app",
+  integrations: [preact()]
 });
