228c46e3d52087b999fd4f23557f266c8f85c056
fjsho <81564274+fjsho@users.noreply.github.com>
2025-10-26 20:57:03 +0900

    fix: ハンバーガーメニューの処理の可読性向上

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,9 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-  document.querySelector('.nav-links').classList.toggle('expanded');
-});
+// ハンバーガーメニューの動作
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+if (hamburger && navLinks) {
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('expanded');
+  });
+}
